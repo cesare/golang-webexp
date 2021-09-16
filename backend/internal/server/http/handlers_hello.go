e2e34be4cfc472cf300ffbd7a856da6f1e5a9300
SAWADA Tadashi <cesare@mayverse.jp>
2021-09-16 21:42:53 +0900

    Separate request handler

---
+++ backend/internal/server/http/handlers_hello.go
@@ -0,0 +1,13 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func hello(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "hello world",
+	})
+}
--- backend/internal/server/http/http.go
+++ backend/internal/server/http/http.go
@@ -1,17 +1,11 @@
 package http
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Engine() *gin.Engine {
 	engine := gin.Default()
-	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
-	})
+	engine.GET("/", hello)
 	return engine
 }
