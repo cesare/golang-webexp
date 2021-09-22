64802fbd28fd2b8d2ca1240f441e902b916991c4
SAWADA Tadashi <cesare@mayverse.jp>
2021-09-22 21:50:31 +0900

    Add configuration structs and loader

---
+++ backend/internal/configs/config.go
@@ -0,0 +1,38 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Config struct {
+	Server ServerConfig
+}
+
+type ServerConfig struct {
+	Bind string
+	Port uint16
+}
+
+type ConfigLoader struct {
+	path string
+}
+
+func (cl *ConfigLoader) Load() (*Config, error) {
+	f, err := os.Open(cl.path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open configuration file %s: %s", cl.path, err.Error())
+	}
+	defer f.Close()
+
+	var config Config
+	decoder := toml.NewDecoder(f)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse configuration file %s: %s", cl.path, err.Error())
+	}
+
+	return &config, nil
+}
