a510ede55de8ca16b71e687cd8cae0172431fd5f
SAWADA Tadashi <cesare@mayverse.jp>
2021-09-29 15:50:52 +0900

    Save visitor name when available

--- backend/internal/server/http/handlers_hello.go
+++ backend/internal/server/http/handlers_hello.go
@@ -1,13 +1,26 @@
 package http
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func hello(c *gin.Context) {
+	session := sessions.Default(c)
+
+	name := c.Query("name")
+	if name != "" {
+		session.Set("last-visited", name)
+		session.Save()
+	} else {
+		name = session.Get("last-visited").(string)
+	}
+
+	message := fmt.Sprintf("hello, %s", name)
 	c.JSON(http.StatusOK, gin.H{
-		"message": "hello world",
+		"message": message,
 	})
 }
