1d22984e84831bec117eaf0260844461fa3e3a91
SAWADA Tadashi <cesare@mayverse.jp>
2021-10-08 22:37:07 +0900

    Enable to start authorization

---
+++ frontend/pages/auth/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>test for echo</p>
+</template>
+
+<script lang="ts">
+import { NuxtHTTPInstance } from '@nuxt/http'
+import Vue from 'vue'
+
+interface AuthStartResponse {
+  authorizationUri: string,
+}
+
+export default {
+  async fetch() {
+    const response: AuthStartResponse = await this.$http.$get("http://localhost:8000/auth")
+    const authorizationUri = response.authorizationUri
+    window.location.assign(authorizationUri)
+  },
+  fetchOnServer: false,
+}
+</script>
