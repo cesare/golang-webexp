58cfd64ad6dd76fb487ce72b46e4de75f10b8b3e
SAWADA Tadashi <cesare@mayverse.jp>
2021-10-21 22:41:02 +0900

    Setup database connection

--- backend/internal/server/http/middlewares/database.go
+++ backend/internal/server/http/middlewares/database.go
@@ -1,13 +1,21 @@
 package middlewares
 
 import (
+	"database/sql"
 	"webexp/internal/configs"
 
 	"github.com/gin-gonic/gin"
 )
 
 func DatabaseMiddleware(config *configs.Config) (gin.HandlerFunc, error) {
+	db, err := sql.Open("postgres", config.Database.ConnectionString())
+	if err != nil {
+		return nil, err
+	}
+
 	handler := func(c *gin.Context) {
+		c.Set("database", db)
+		c.Next()
 	}
 	return handler, nil
 }
