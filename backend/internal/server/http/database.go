b664b6712cba39f320404ca6df5818dd8f464b1b
SAWADA Tadashi <cesare@mayverse.jp>
2021-10-23 18:32:50 +0900

    Panic when database object missing

--- backend/internal/server/http/database.go
+++ backend/internal/server/http/database.go
@@ -2,21 +2,11 @@ package http
 
 import (
 	"database/sql"
-	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
-func database(c *gin.Context) (*sql.DB, error) {
-	v, exists := c.Get("database")
-	if !exists {
-		return nil, fmt.Errorf("database missing")
-	}
-
-	db, ok := v.(*sql.DB)
-	if !ok {
-		return nil, fmt.Errorf("unknown database object")
-	}
-
-	return db, nil
+func database(c *gin.Context) *sql.DB {
+	v := c.MustGet("database")
+	return v.(*sql.DB)
 }
