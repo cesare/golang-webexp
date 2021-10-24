7c835c87631bf2aea624e8a725d0918547d31e84
SAWADA Tadashi <cesare@mayverse.jp>
2021-10-24 20:36:05 +0900

    Add empty identity repository

--- backend/internal/identities/identities.go
+++ backend/internal/identities/identities.go
@@ -1,10 +1,33 @@
 package identities
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Identity struct {
 	Id                 string
 	PrividerIdentifier string
 	Alive              bool
 	RegisterdAt        time.Time
+}
+
+type IdentityRepository struct {
+	db *sql.DB
+}
+
+func NewIdentityRepository(db *sql.DB) *IdentityRepository {
+	return &IdentityRepository{db: db}
+}
+
+func (r *IdentityRepository) Find(id string) (*Identity, error) {
+	return nil, nil
+}
+
+type RegistrationDataset struct {
+	ProviderIdentifier string
+}
+
+func (r *IdentityRepository) Register(ds *RegistrationDataset) (*Identity, error) {
+	return nil, nil
 }
