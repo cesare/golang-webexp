a28e2994279afe3c0f5af3afbf7e56316bf05638
SAWADA Tadashi <cesare@mayverse.jp>
2021-10-26 10:31:16 +0900

    Enable to register identity

--- backend/internal/identities/identities.go
+++ backend/internal/identities/identities.go
@@ -35,10 +35,35 @@ func (r *IdentityRepository) Find(id string) (*Identity, error) {
 	return &identity, nil
 }
 
+func (r *IdentityRepository) FindByProviderIdentifier(identifier string) (*Identity, error) {
+	identity := Identity{}
+	query := "select id, provider_identifier, alive, registered_at from identities where provider_identifier = $1"
+	row := r.db.QueryRow(query, identifier)
+	err := row.Scan(&identity.Id, &identity.PrividerIdentifier, &identity.Alive, &identity.RegisterdAt)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &identity, nil
+}
+
 type RegistrationDataset struct {
 	ProviderIdentifier string
 }
 
 func (r *IdentityRepository) Register(ds *RegistrationDataset) (*Identity, error) {
-	return nil, nil
+	query := `insert into identities (id, provider_identifier) values (gen_random_uuid(), $1)
+		returning id, provider_identifier, alive, registered_at`
+	row := r.db.QueryRow(query, ds.ProviderIdentifier)
+
+	identity := Identity{}
+	err := row.Scan(&identity.Id, &identity.PrividerIdentifier, &identity.Alive, &identity.RegisterdAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &identity, nil
 }
